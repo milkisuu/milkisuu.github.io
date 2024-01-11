5004fde69d814184c9b67bb0cafddc69fbecb32e
Denise <153671467+milkisuu@users.noreply.github.com>
2024-01-11 15:41:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+    function hearts() {
+        const container = document.querySelector('.container');
+    
+        const creat = document.createElement('div');
+        creat.classList.add('hearts');
+        creat.innerHTML = '♡';
+        creat.style.left = Math.random() * 100 + 'vw';
+        creat.style.animationDuration = Math.random() * 5 + 4 + 's';
+    
+        container.appendChild(creat);
+    
+        setTimeout(() => {
+            creat.remove();
+        }, 3000);
+    }
+    
+    setInterval(hearts, 100);
